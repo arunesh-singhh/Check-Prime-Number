0c47fc63def306c2c2f9767930ecd3d6a1c73f02
Arunesh Singh <125174712+arunesh-singhh@users.noreply.github.com>
2023-07-14 17:34:37 +0530

    Update Check_Prime.java

--- Check_Prime.java
+++ Check_Prime.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 public class Check_Prime {
 
     public static void main(String[] args) {
-        try(Scanner s = new Scanner(System.in)) {
-            int n = s.nextInt();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
             
-            int div = 2;
-            while(div <= n/2){
-                if(n % div == 0){
-                    System.out.println("not prime");
-                    return;
-                }
-                div = div + 1;
+        int div = 2;
+        while(div <= n/2){
+            if(n % div == 0){
+                System.out.println("not prime");
+                return;
             }
-            System.out.println("prime number");
+            div = div + 1;
         }
+        System.out.println("prime number");
     }
     
 }
